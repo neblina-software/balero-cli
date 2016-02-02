4b3d30ecdb6403844bb05498011e46806cd90ac8
Anibal Gomez <anibalgomez@balerocms.com>
2016-02-01 20:34:40 -0600

    Zip File Download

--- src/com/neblina/balero/Application.java
+++ src/com/neblina/balero/Application.java
@@ -1,8 +1,22 @@
+/**
+ * Balero CMS Project: Proyecto 100% Mexicano de código libre.
+ * Página Oficial: http://www.balerocms.com
+ *
+ * @author Anibal Gomez <anibalgomez@icloud.com>
+ * @copyright Copyright (C) 2016 Neblina Software. Derechos reservados.
+ * @license Licencia BSD; vea LICENSE.txt
+ */
+
 package com.neblina.balero;
+
+import com.neblina.balero.classes.URLConnectionReader;
+import java.io.IOException;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Starting Updater...");
+        URLConnectionReader urlConnectionReader = new URLConnectionReader();
+        urlConnectionReader.downloadZipFile(System.getProperty("user.dir"));
     }
 }
